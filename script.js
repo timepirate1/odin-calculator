94bf44a4962b251a05bf506e689c24caac65ed22
timepirate1 <73796656+timepirate1@users.noreply.github.com>
2025-11-26 10:13:52 +0530

    Create initial structure and functions of calculator

---
+++ script.js
@@ -0,0 +1,45 @@
+let number1 = NaN;
+let number2 = NaN;
+let operator = '';
+
+function add(a,b){
+    return  a+b;
+}
+function subtract(a,b){
+    return  a-b;
+}
+function mulitply(a,b){
+    return  a*b;
+}
+function divide(a,b){
+    return  a/b;
+}
+
+function operate(number1,number2,operator){
+    let result = 0;
+    if (operator ==='+'){
+        result = add(number1,number2);
+    }   
+    if (operator ==='-'){
+        result = subtract(number1,number2);
+    }   
+    if (operator ==='*'){
+        result = mulitply(number1,number2);
+    }   
+    if (operator ==='/'){
+        result = divide(number1,number2);
+    }   
+    console.log(result);
+}
+
+
+
+// note we do + before prompt to ensure the value is numeric and not taken as a string a
+// as that can conflict with the addition function and append instead of add the numbers
+
+// number1 = + prompt('enter first number'); 
+// operator = prompt('operation');
+// number2 = + prompt('enter 2nd number');
+
+
+//operate(number1,number2,operator)
