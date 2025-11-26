c7660b4de86557f904678899c9dd866980816fdf
timepirate1 <73796656+timepirate1@users.noreply.github.com>
2025-11-26 15:20:06 +0530

    Add basic input and operation functionality to calculator

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-let number1 = NaN;
-let number2 = NaN;
+let number1 = null;
+let number2 = null;
 let operator = '';
 
 function add(a,b){
-    return  a+b;
+    return  Number(a)+Number(b);
 }
 function subtract(a,b){
     return  a-b;
 }
-function mulitply(a,b){
+function multiply(a,b){
     return  a*b;
 }
 function divide(a,b){
     return  a/b;
 }
 
+
+
+
+//displays the input on screen
+const display =document.querySelector('.display');
+let input = null;
+
+//handles calculations
 function operate(number1,number2,operator){
     let result = 0;
     if (operator ==='+'){
@@ -24,36 +32,66 @@ function operate(number1,number2,operator){
         result = subtract(number1,number2);
     }   
     if (operator ==='*'){
-        result = mulitply(number1,number2);
+        result = multiply(number1,number2);
     }   
     if (operator ==='/'){
         result = divide(number1,number2);
     }   
+    display.innerText = result;
     console.log(result);
+    
 }
 
 
+//used to display input 
+function displayInput(ip){
+    input += ip;
+    display.innerText += ip ;
 
-
-
-const display =document.querySelector('.display');
-function displayInput(input){
-   
-    display.innerText += input ;
 }
 
+//used to take input from numeric keys
 let inputButtons = document.querySelectorAll('.num-btn');
 inputButtons.forEach(button => {
     button.addEventListener('click', ()=>{
-        const input = button.textContent;
-        displayInput(input);
+        displayInput(button.textContent);
     })
 });
 
 
+// handles operation input and calling operations
+let operationButtons = document.querySelectorAll('.op-btn');
+operationButtons.forEach(but=>{
+    but.addEventListener('click',()=>{
+        // stores input as number 1
+        number1 = input;
+        operator = but.textContent;
+        console.log(operator);
+        displayInput(but.textContent);
+        //resets input
+        input = null; 
+        console.log(number1);
+        // handles equal button and operation calling
+        document.querySelector('#equal-btn').addEventListener('click',()=>{
+            number2 = input;
+            console.log(number2);
+            operate(number1,number2,operator);
+            input = '';
+           })
+    })
+});
+
+
+
+//clears the screen
 const clearScreen = document.querySelector('.clear-btn');
 clearScreen.addEventListener('click',()=>{
-    display.innerText= ' ';
+    display.innerText= ' '; 
+    number1 = null;
+    number2 = null;
+    input = null;
+    operator = '';
+
 })
 
 
